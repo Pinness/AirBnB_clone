9ea384e2572ed2768cec91b49c4d3e3f60fa7596
Happiness <adamhappinessjw@gmail.com>
2024-05-23 11:41:42 -0700

    Update

--- console.py
+++ console.py
@@ -1,26 +1,47 @@
-import cmd
+import cmd 
 
-class MyConsole(cmd.Cmd):
+class MyConsole (cmd.Cmd):
+    def cmdloop(self):
+        self.prompt = 'Piness >>>  '
+        return cmd.Cmd.cmdloop(self)
+
+
+    #prompt = 'Piness >>>  '   #display prompt
+    #def do_quit(self, arg):
+
     def do_quit(self, arg):
-        return True #exit when the user type quit
-
+        return True
 
     def do_EOF(self, arg):
+        """ Exits the program"""
+        print()
         return True # exit program wshen user trigger EOF
 
+
     def do_help(self, arg):
-        print('Welcome to MyConsole')
-        print('Available commands:\n    quit - Exit the program\n    EOF - Exit the program')
+        if arg == 'quit':
+            print('Quit command to exit the program')
+        elif arg == '':
+            print('''Welcome to MyConsole!\n\nDocumented commands (type help <topic>):
+            ========================================')
+                    'Available commands:\n    help - Provide assistance\n    quit - Exit the program\n    EOF - Exit the program''')
 
-    def help_quit(self):
-        print('Quit command to exit the program')
+         
+        else:
+             cmd.Cmd.do_help(self, arg)
 
-    def prompt(self):
-        return 'Piness >>>  ' #cusom prompt
 
-    def do_emptyline(self):
+    def emptyline(self):
+        """ Enters a new line"""
+
         pass #do nothing when an empty line is entered
 
 
+    #handle wrong commands
+    def default(self, arg):
+        print(f"Unknown Command: {arg}")
+
+
 if __name__ == '__main__':
-        MyConsole().cmdloop()
+    MyConsole().cmdloop()
+
